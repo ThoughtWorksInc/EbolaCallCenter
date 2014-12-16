09e82b44791bd869992fb34947619224fd8d8ebd
Igor Lilic <igorlilic@gmail.com>
2014-12-15 19:30:52 -0500

    Get rid of coffee noise

--- karma.conf.js
+++ karma.conf.js
@@ -21,11 +21,8 @@ module.exports = function(config) {
       'client/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'client/bower_components/lodash/dist/lodash.compat.js',
       'client/app/app.js',
-      'client/app/app.coffee',
       'client/app/**/*.js',
-      'client/app/**/*.coffee',
       'client/components/**/*.js',
-      'client/components/**/*.coffee',
       'client/app/**/*.jade',
       'client/components/**/*.jade',
       'client/app/**/*.html',
@@ -35,7 +32,6 @@ module.exports = function(config) {
     preprocessors: {
       '**/*.jade': 'ng-jade2js',
       '**/*.html': 'html2js',
-      '**/*.coffee': 'coffee',
     },
 
     ngHtml2JsPreprocessor: {
