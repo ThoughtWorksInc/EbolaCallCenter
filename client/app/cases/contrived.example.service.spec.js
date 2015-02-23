da69f95290d5b78c63af167789960adda7955ce4
Timothy Cochran <timothy.cochran@gmail.com>
2015-02-22 22:58:33 -0500

    TC adding test output to jasmine stuff

--- client/app/cases/contrived.example.service.spec.js
+++ client/app/cases/contrived.example.service.spec.js
@@ -9,7 +9,11 @@ describe('ContrivedExampleService', function () {
 
   it('should concat strings', function () {
     var result = ContrivedExampleService.concatStrings("a very silly ", "example");
-    expect(result).toEqual("a very silly example");
+    expect(result).toEqual("a very wrong example");
   });
+
+  // PhantomJS 1.9.8 (Mac OS X) ContrivedExampleService should concat strings FAILED
+  // Expected 'a very silly example' to equal 'a very wrong example'.
+  // PhantomJS 1.9.8 (Mac OS X): Executed 5 of 5 (1 FAILED) (0.002 secs / 0.019 secs)
 
 });
