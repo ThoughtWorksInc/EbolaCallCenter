c7f32bc6b3cbccfb7316219a3f78b1a4af007ca4
Timothy Cochran <timothy.cochran@gmail.com>
2015-02-23 00:56:34 -0500

    TC shortened examples

--- client/app/cases/contrived.example.service.js
+++ client/app/cases/contrived.example.service.js
@@ -2,7 +2,6 @@ ContrivedExampleService = {};
 ContrivedExampleService.squareNumber = function(n) {
     return n * n;
 }
-
 ContrivedExampleService.concatStrings = function(string1, string2) {
     return string1 + string2;
 }
--- e2e/main/case.spec.js
+++ e2e/main/case.spec.js
@@ -11,66 +11,38 @@ describe('Case View', function() {
 
     browser.get('/');
     newCasePage = require('./new-case.po');
-    casesPage = require('./cases.po');
-  });
+  });  
 
-  it('should validate short description', function() {
-    newCasePage.submit();
+
+  it('should validate short description - 1', function() {
+    var formEl = element(by.css('form'));
+    var submitButton = formEl.element(by.css('button'));
+    submitButton.click();
+
     expect(newCasePage.alertEl.getText()).toContain('Case Short Description is required');
   });
 
-  it('should validate phone number', function() {
-    newCasePage.submit();
-    expect(newCasePage.alertEl.getText()).toContain('Phone Number is required');
+  it('should validate short description - 2', function() {
+    var formEl = element(by.css('form'));
+    var submitButton = formEl.element(by.css('button'));
+    submitButton.click();
 
-    newCasePage.hwPhoneInput.sendKeys('rubbish');
-    expect(newCasePage.hwPhoneInput.getAttribute('class')).toContain('ng-invalid');
+    expect(newCasePage.alertEl.getText()).toContain('Case Short Description is required');
 
-    newCasePage.hwPhoneInput.clear();
-    newCasePage.hwPhoneInput.sendKeys('123 - ()');
-    expect(newCasePage.hwPhoneInput.getAttribute('class')).toContain('ng-valid');
+    var shortDescriptionInput = formEl.element(by.model('hwCase.shortDescription'));
+    shortDescriptionInput.sendKeys("something");
+    var formEl = element(by.css('form'));
+    formEl.click();
+
+    expect(newCasePage.alertEl.getText()).toNotContain('Case Short Description is required');
   });
 
-
-  it('should validate health worker name', function() {
+  it('should validate short description - 3', function() {
     newCasePage.submit();
-    expect(newCasePage.alertEl.getText()).toContain('Name is required');
+    expect(newCasePage.errorMessages()).toContain('Case Short Description is required');
 
-    newCasePage.setFirstName('Test First Name');
-    expect(newCasePage.alertEl.getText()).toContain('Name is required');
-
-    newCasePage.setLastName('Test Last Name');
-    expect(newCasePage.hwLastNameInput.getAttribute('class')).toContain('ng-valid');
-
-    newCasePage.clearHealthWorker();
-    expect(newCasePage.hwFirstNameInput.getAttribute('class')).toContain('ng-invalid');
+    newCasePage.setShortDescription('something');
+    expect(newCasePage.errorMessages()).toNotContain('Case Short Description is required');
   });
-
-  it('should validate email address', function() {
-    newCasePage.submit();
-    expect(newCasePage.alertEl.getText()).not.toContain('Email must be valid');
-    
-    newCasePage.hwEmailInput.sendKeys('invalidemail');    
-    newCasePage.submit();
-    expect(newCasePage.hwEmailInput.getAttribute('class')).toContain('ng-invalid');
-
-    newCasePage.hwEmailInput.clear();    
-    newCasePage.hwEmailInput.sendKeys('validemail@gmail.com');
-    newCasePage.submit();
-    expect(newCasePage.hwEmailInput.getAttribute('class')).toContain('ng-valid');
-  });
-
-  it('should create new case with assigned health worker', function() {
-    newCasePage.setShortDescription('Test');
-    newCasePage.notesInput.sendKeys('Test Notes');
-    newCasePage.setFirstName('Test First Name');
-    newCasePage.setLastName('Test Last Name');
-    newCasePage.hwPhoneInput.sendKeys('123456789');
-    newCasePage.hwEmailInput.sendKeys('TestEmail@gmail.com');
-    newCasePage.submit();
-
-    expect(casesPage.casesLi.getText()).toMatch(['Case #CASE-100: Test - Test Notes - Test First Name - Test Last Name - 123456789 - TestEmail@gmail.com'])
-  });
-
 
 });
